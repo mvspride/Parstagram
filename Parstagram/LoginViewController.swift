fbb931e37c30387131a9962ac27abadb7e679c68
Pride Mbabit <pridembabit@gmail.com>
2022-10-18 14:27:44 -0400

    added code for Parstagram

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Pride Mbabit on 10/16/22.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func OnSignUp(_ sender: UIButton) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{
+            (success,error) in if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    @IBAction func OnSignIn(_ sender: UIButton) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username,password: password){
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Parstagram
+  pod 'AlamofireImage'
+  pod 'parse'
+  platform :ios, '8.0'
+  use_frameworks!    # Instruct CocoaPods to use dynamic frameworks instead of static libraries for Pods
+  ...
+
+
+end
